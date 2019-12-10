542f002c6feeabf5c20e4a4b5029fa7483fc9885
unknown <ikoliogi@gmail.com>
2019-12-10 17:07:56 +0200

    search input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {RouterModule, ROUTES} from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
